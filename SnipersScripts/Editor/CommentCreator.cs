ee32d13260da5849e998e179ac6b165aa8187ab1
Justin Hooker <justinhooker1@gmail.com>
2026-08-04 18:59:20 -0400

    comment and materialmaker script

---
+++ SnipersScripts/Editor/CommentCreator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEditor.Compilation;
+using UnityEngine;
+
+namespace SnipersScripts.Editor
+{
+    [CreateAssetMenu(fileName = "Comment", menuName = "SnipersScripts/Comment")]
+    public class CommentSO : ScriptableObject
+    {
+        [SerializeField]
+        [TextArea(1, 20)]
+        private string commentText = string.Empty;
+    }
+    [AddComponentMenu("SnipersScripts/Comment")]
+    public class  CommentComponent : MonoBehaviour
+    {
+        [SerializeField]
+        [TextArea(1, 5)]
+        private string commentText = string.Empty;
+    }
+}
