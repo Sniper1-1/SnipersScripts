42db64f75cd0f169061f50eae9ded8d3594eb8c3
Justin Hooker <justinhooker1@gmail.com>
2026-08-04 12:11:15 -0400

    network patch

---
+++ SnipersScripts/Patches/NetworkPatch.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using HarmonyLib;
+using SnipersScripts;
+using Unity.Netcode;
+using UnityEngine;
+
+[HarmonyPatch(typeof(NetworkManager))]
+internal static class NetworkPrefabPatch1
+{
+    private static readonly string MOD_GUID = MyPluginInfo.PLUGIN_GUID;
+
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(NetworkManager.SetSingleton))]
+    private static void RegisterPrefab()
+    {
+        var prefab = new GameObject(MOD_GUID + " Prefab");
+        prefab.hideFlags |= HideFlags.HideAndDontSave;
+        Object.DontDestroyOnLoad(prefab);
+        var networkObject = prefab.AddComponent<NetworkObject>();
+        networkObject.GlobalObjectIdHash = GetHash(MOD_GUID);
+
+        NetworkManager.Singleton.PrefabHandler.AddNetworkPrefab(prefab);
+        return;
+
+        static uint GetHash(string value)
+        {
+            return value?.Aggregate(17u, (current, c) => unchecked((current * 31) ^ c)) ?? 0u;
+        }
+    }
+}
