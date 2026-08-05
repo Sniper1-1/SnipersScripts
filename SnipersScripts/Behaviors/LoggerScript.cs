e8e41143f127d4dee4bd9ae6a49280aa6f35cb25
Justin Hooker <justinhooker1@gmail.com>
2026-08-05 12:19:04 -0400

    can now log on awake

--- SnipersScripts/Behaviors/LoggerScript.cs
+++ SnipersScripts/Behaviors/LoggerScript.cs
@@ -6,6 +6,24 @@ namespace SnipersScripts.Behaviors
     public class  LoggerScript: MonoBehaviour
     {
         private static readonly string MESSAGE_STARTER = "SnipersScripts' LoggerScript says: ";
+
+        [Tooltip("The message to log to the console on Awake. Left empty, logs can only be printed by calling the print methods from other events.")]
+        public string log=string.Empty;
+        [Tooltip("The channel to log message to if log is not empty")]
+        public type logType;
+        public enum type { Debug, Info, Warning, Error, Fatal }
+
+        private void Awake()
+        {
+            if (!string.IsNullOrEmpty(log))
+            {
+                if (logType == type.Debug) { printDebug(log); }
+                if (logType == type.Info) { printInfo(log); }
+                if (logType == type.Warning) { printWarning(log); }
+                if (logType == type.Error) { printError(log); }
+                if (logType == type.Fatal) { printFatal(log); }
+            }
+        }
         public void printDebug(string message) { SnipersScripts.Logger.LogDebug(MESSAGE_STARTER+message); }
         public void printInfo(string message) { SnipersScripts.Logger.LogInfo(MESSAGE_STARTER+message); }
         public void printWarning(string message) { SnipersScripts.Logger.LogWarning(MESSAGE_STARTER+message); }
