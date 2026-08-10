0c550c73922d36c2452ca59a7d3b3fc344e2ecea
Justin Hooker <justinhooker1@gmail.com>
2026-08-10 18:22:50 -0400

    item drop distance can be overridden

---
+++ SnipersScripts/Behaviors/ItemDropDistanceOverride.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SnipersScripts.Patches;
+using UnityEngine;
+
+namespace SnipersScripts.Behaviors
+{
+    [AddComponentMenu("SnipersScripts/ItemDropDistanceOverride")]
+    public class ItemDropDistanceOverride: MonoBehaviour
+    {
+        [Tooltip("The distance from which items can be dropped.")]
+        public float itemDropDistance = ItemDropDistancePatch.VanillaRaycastDistance;
+        [Tooltip("If true, the override will be applied on Start. If false, you can call ApplyDropDistanceOverride() manually.")]
+        public bool applyOverrideOnStart = true;
+
+        private void Start()
+        {
+            if (applyOverrideOnStart)
+            {
+                ApplyDropDistanceOverride(itemDropDistance);
+            }
+        }
+
+        /// <summary>
+        /// Sets a custom item drop distance.
+        /// </summary>
+        /// <param name="distance">The distance from which items can be dropped.</param>
+        public void ApplyDropDistanceOverride(float distance)
+        {
+            ItemDropDistancePatch.CustomRaycastDistance = distance;
+            SnipersScripts.Logger.LogDebug($"Item drop distance override applied: {distance}");
+        }
+
+        // Reset to vanilla distance when this component is destroyed
+        private void OnDestroy()
+        { 
+            ItemDropDistancePatch.CustomRaycastDistance = ItemDropDistancePatch.VanillaRaycastDistance;
+            SnipersScripts.Logger.LogDebug($"Item drop distance override removed, reset to {ItemDropDistancePatch.VanillaRaycastDistance}.");
+        }
+    }
+}
---
+++ SnipersScripts/Patches/GrabbableObjectPatches.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace SnipersScripts.Patches
+{
+    [HarmonyPatch]
+    public class ItemDropDistancePatch
+    {
+        public static float CustomRaycastDistance = 80f;
+        public static readonly float VanillaRaycastDistance = 80f;
+
+        [HarmonyTargetMethods] // all 3 need to be patched for dropping items
+        private static IEnumerable<System.Reflection.MethodBase> TargetMethods()
+        {
+            yield return AccessTools.Method(typeof(GrabbableObject), nameof(GrabbableObject.GetItemFloorPosition));
+            yield return AccessTools.Method(typeof(GrabbableObject), nameof(GrabbableObject.GetPhysicsRegionOfDroppedObject));
+            yield return AccessTools.Method(typeof(GrabbableObject), nameof(GrabbableObject.GetPhysicsRegionOfDroppedObjectSynced));
+        }
+
+        [HarmonyTranspiler]
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, MethodBase original)
+        {
+            var matcher = new CodeMatcher(instructions);
+            var originalDropDistance = new CodeMatch(OpCodes.Ldc_R4, VanillaRaycastDistance); // find where vanilla drop distance value is mentioned
+            var raycastDistanceField = AccessTools.Field(typeof(ItemDropDistancePatch), nameof(CustomRaycastDistance)); //prepares a field that can be put in place of the vanilla drop distance inline value
+
+            matcher.MatchForward(false, originalDropDistance); //go to first vanilla drop distance
+
+            while (matcher.IsValid)
+            {                
+                matcher.SetInstructionAndAdvance(new CodeInstruction(OpCodes.Ldsfld, raycastDistanceField)); //replace with new distance field that can be dynamically changed
+                SnipersScripts.Logger.LogDebug("Patched raycast distance.");
+                matcher.MatchForward(false, originalDropDistance); //find subsequent vanilla drop distances
+            }
+
+            return matcher.InstructionEnumeration();
+        }
+    }
+}
