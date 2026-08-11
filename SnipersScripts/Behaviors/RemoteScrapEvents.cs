5869dd5611bebbb4e3f7868bfef8ced4220125f2
Justin Hooker <justinhooker1@gmail.com>
2026-08-11 17:37:19 -0400

    RemoteScrapEvents script made

---
+++ SnipersScripts/Behaviors/RemoteScrapEvents.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace SnipersScripts.Behaviors
+{
+    [AddComponentMenu("SnipersScripts/RemoteScrapEvents")]
+    public class RemoteScrapEvents: MonoBehaviour
+    {
+        [Tooltip("Event to run when the remote scrap item is clicked")]
+        public UnityEngine.Events.UnityEvent onRemoteClicked;
+    }
+}
---
+++ SnipersScripts/Patches/RemoteScrapPatch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using SnipersScripts.Behaviors;
+using UnityEngine;
+
+namespace SnipersScripts.Patches
+{
+    [HarmonyPatch(typeof(RemoteProp))]
+    internal class RemoteScrapPatch
+    {
+        // invokes all the RemoteScrapEvents when the remote scrap item is clicked
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(RemoteProp), nameof(RemoteProp.ItemActivate))]
+        private static void ItemActivate_Postfix(RemoteProp __instance)
+        {
+            var remoteScrapEvents = GameObject.FindObjectsOfType<RemoteScrapEvents>();
+            foreach (var remoteScrapEvent in remoteScrapEvents)
+            {
+                remoteScrapEvent.onRemoteClicked?.Invoke();
+            }
+        }
+    }
+}
