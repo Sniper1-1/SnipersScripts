73a8e3a9bd4624a6da4f4227a6e43b662b7833b2
Justin Hooker <justinhooker1@gmail.com>
2026-08-18 15:06:33 -0400

    started shipcontroller
    magnet code

---
+++ SnipersScripts/Behaviors/ShipController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using GameNetcodeStuff;
+using UnityEngine;
+
+namespace SnipersScripts.Behaviors
+{
+    [AddComponentMenu("SnipersScripts/ShipController")]
+    public class ShipController : MonoBehaviour
+    {
+        // Registry of every active ship controller currently loaded
+        internal static readonly List<ShipController> ActiveControllers = new List<ShipController>();
+        private void OnEnable() { ActiveControllers.Add(this); }
+        private void OnDisable() { ActiveControllers.Remove(this); }
+
+        [Header("Magnet")]
+        public UnityEngine.Events.UnityEvent onMagnetEnable;
+        public UnityEngine.Events.UnityEvent onMagnetDisable;
+        public UnityEngine.Events.UnityEvent onMagnetToggle;
+        public void SetMagnetPowered(bool powered)
+        {
+            if (powered != StartOfRound.Instance.magnetOn) { CycleMagnetPower(); }
+        }
+        public void CycleMagnetPower()
+        {
+            CycleMagnetPower(true);
+        }
+        private void CycleMagnetPower(bool powered)
+        {
+            StartOfRound.Instance.SetMagnetOn(powered);
+            StartOfRound.Instance.magnetLever.TriggerAnimation(StartOfRound.Instance.localPlayerController);
+        }
+        
+    }
+}
---
+++ SnipersScripts/Patches/ShipPatches.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using SnipersScripts.Behaviors;
+
+namespace SnipersScripts.Patches
+{
+    [HarmonyPatch]
+    internal class ShipPatches
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(StartOfRound),nameof(StartOfRound.SetMagnetOn))]
+        private static void MagnetTogglePrefix(bool on)
+        {
+            if (on)
+            {
+                foreach (ShipController controller in ShipController.ActiveControllers) { controller.onMagnetEnable.Invoke(); }
+            }
+            else
+            {
+                foreach (ShipController controller in ShipController.ActiveControllers) { controller.onMagnetDisable.Invoke(); }
+            }
+            foreach (ShipController controller in ShipController.ActiveControllers) { controller.onMagnetToggle.Invoke(); }
+        }
+    }
+}
