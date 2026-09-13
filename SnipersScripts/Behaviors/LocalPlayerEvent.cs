2166fc72c04d1c22a51e2696b353483fae245b68
Justin Hooker <justinhooker1@gmail.com>
2026-09-13 18:28:56 -0400

    local player event script

---
+++ SnipersScripts/Behaviors/LocalPlayerEvent.cs
@@ -0,0 +1,20 @@
+﻿using GameNetcodeStuff;
+using UnityEngine;
+
+namespace SnipersScripts.Behaviors
+{
+    [AddComponentMenu("SnipersScripts/LocalPlayerEvent")]
+    internal class LocalPlayerEvent: MonoBehaviour
+    {
+        [Tooltip("Runs on the local player.")]
+        public UnityEngine.Events.UnityEvent<PlayerControllerB> localPlayerEvent;
+
+        /// <summary>
+        /// Invokes the localPlayerEvent on the local player
+        /// </summary>
+        public void InvokeLocalPlayerEvent()
+        {
+            localPlayerEvent.Invoke(StartOfRound.Instance.localPlayerController);
+        }
+    }
+}
