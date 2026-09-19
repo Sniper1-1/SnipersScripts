899b60234cbaeba00d07850b79d482e489ffcde8
Justin Hooker <justinhooker1@gmail.com>
2026-09-18 20:18:53 -0400

    random material script

---
+++ SnipersScripts/Behaviors/RandomMaterialSelector.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SnipersScripts.Behaviors
+{
+    [AddComponentMenu("SnipersScripts/RandomMaterialSelector")]
+    public class RandomMaterialSelector : MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("If true, only use the level seed. This means that additional objects with the same possibleMaterials list will end up with the same result. If false, additional randomization is used.")]
+        private bool onlyUseLevelSeed = false;
+        [SerializeField]
+        [Tooltip("If true, all mesh renderers will share the same outcome. If false, each mesh renderer will have its own outcome.")]
+        private bool renderersShareOutcome = false;
+        [SerializeField]
+        [Tooltip("The mesh renderers to randomize the materials of.")]
+        private List<MeshRenderer> meshRenderers = new List<MeshRenderer>();
+        [SerializeField]
+        [Tooltip("The weighted list of materials to choose from.")]
+        private List<RandomMaterialSelectorData> possibleMaterials = new List<RandomMaterialSelectorData>();
+        public void Start()
+        {
+            // seed is either just the level seed, or it is the combination of the level seed and the position of this object
+            int seed = onlyUseLevelSeed ? RoundManager.Instance.playersManager.randomMapSeed : RoundManager.Instance.playersManager.randomMapSeed+(int)(this.transform.position.x*100)+ (int)(this.transform.position.y*100)+ (int)(this.transform.position.z*100);
+
+            System.Random random = new System.Random(seed);
+            if (renderersShareOutcome)
+            {   
+                //only randomize once and then apply that to all renderers
+                RandomMaterialSelectorData randomlySelectedMaterials = WeightedSelector<RandomMaterialSelectorData>.SelectWeightedRandom(possibleMaterials, random.Next());
+                foreach (MeshRenderer meshRenderer in meshRenderers)
+                {
+                    if (meshRenderer != null && randomlySelectedMaterials.materials.Count > 0)
+                    {
+                        meshRenderer.sharedMaterials = randomlySelectedMaterials.materials.ToArray();
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < meshRenderers.Count; i++)
+                {
+                    if (meshRenderers[i] != null)
+                    {
+                        //each renderer gets its own randomization
+                        RandomMaterialSelectorData randomlySelectedMaterials = WeightedSelector<RandomMaterialSelectorData>.SelectWeightedRandom(possibleMaterials, random.Next());
+                        if (randomlySelectedMaterials.materials.Count > 0)
+                        {
+                            meshRenderers[i].sharedMaterials = randomlySelectedMaterials.materials.ToArray();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class RandomMaterialSelectorData: IWeighted
+    {
+        [Tooltip("The materials applied to the mesh renderer if chosen")]
+        public List<Material> materials = new List<Material>();
+        [Min(0.0f)]
+        public float weight;
+
+        public float Weight => weight;
+    }
+}
---
+++ SnipersScripts/Utilities/WeightedSelector.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface IWeighted
+{
+    float Weight { get; }
+}
+
+public abstract class WeightedSelector<T> where T : IWeighted
+{
+    /// <summary>
+    /// Takes in a list of weighted items and uses the seed to select a random item based on the weight.
+    /// </summary>
+    /// <param name="items">The list of weighted items to select from.</param>
+    /// <param name="seed">The seed for the random number generator.</param>
+    /// <returns>A randomly selected item from the list based on weight.</returns>
+    public static T SelectWeightedRandom(IList<T> items, int seed)
+    {
+        System.Random random = new(seed);
+
+        if (items == null || items.Count == 0) return default;
+
+        // Add up all weights
+        float totalWeight = 0f;
+        for (int i = 0; i < items.Count; i++)
+        {
+            totalWeight += Math.Max(0f, items[i].Weight);
+        }
+
+        if (totalWeight <= 0f) return default;
+
+        // System.Random.NextDouble() returns a value between 0.0 and 1.0. Multiply by total weight to get a value between 0 and totalWeight
+        double randomValue = random.NextDouble() * totalWeight;
+
+        // Actually select item based on weight
+        double currentCumulativeWeight = 0f;
+        for (int i = 0; i < items.Count; i++)
+        {
+            if (items[i].Weight <= 0f) continue; // skip current item if its weight is zero or negative
+
+            currentCumulativeWeight += items[i].Weight; // add the current item's weight in
+
+            // and check if the random number picked above is less than or equal to the cumulative weight of the current item
+            // (example: 2 items with 50 weight means 100 total. if randomValue is 60, the first item's cumulative weight is 50,
+            // so it's not selected, but the second one's cumulative is 100, so it is)
+            if (randomValue <= currentCumulativeWeight) 
+            {
+                return items[i];
+            }
+        }
+
+        return items[items.Count - 1];
+    }
+}
